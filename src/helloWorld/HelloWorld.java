04669fc3d5d0fa8f97d55ac771de0c29d8e7e564
fedor.kondratev <fedor.kondratev@AMS-GYM-EDV1-12.gym.ams.local>
2019-11-04 18:27:09 +0100

    rechnen

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,34 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.print("Hallo");
+         System.out.print(" ");
+         System.out.print("du");
+         System.out.print(" ");
+         System.out.println("!");
+         //
+         // Textverkettung
+         System.out.println("Hallo" + " " + "du" + " " + "!");
+         //
+         System.out.println(120);
+         System.out.println(1);
+         System.out.println(2);
+         //
+         System.out.println(1*2);
+         System.out.println(1+2);
+         //
+         System.out.println(3-2);
+         System.out.println(9/4);
+         System.out.println(9/4.2);
+         System.out.println(9/4F);
+         System.out.println(9D/4);
+         //
+         System.out.println((1 + 1) + "Ergebnis");
+         System.out.println("1 + 1 = " + (1 + 1));
+        
+	}
+
+}
