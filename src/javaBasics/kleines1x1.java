2771d62eb074440f2f170875c70c4a0c92079b90
fedor.kondratev <fedor.kondratev@AMS-GYM-EDV1-12.gym.ams.local>
2019-11-11 18:49:18 +0100

    1x1

--- src/javaBasics/kleines1x1.java
+++ src/javaBasics/kleines1x1.java
@@ -3,8 +3,8 @@ package javaBasics;
 public class kleines1x1 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10; i++) {
-			for (int j = 1; j <= 10; j++) {
+		for (int j = 1; j <= 10; j++) {
+			for (int i = 1; i <= 10; i++) {
 				System.out.println((i) + "x" + (j) + "=" + (i * j));
 			}
 		}
