8e32d5b2ec4b8e9f29ddd72ea34335f94b2bda33
fedor.kondratev <fedor.kondratev@AMS-GYM-EDV1-12.gym.ams.local>
2019-11-11 17:52:31 +0100

    rechnungen

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -28,7 +28,58 @@ public class HelloWorld {
          //
          System.out.println((1 + 1) + "Ergebnis");
          System.out.println("1 + 1 = " + (1 + 1));
+         //
+         System.out.println(Short.MIN_VALUE);
+         System.out.println(Short.MAX_VALUE);
+         
+         //short - 2byte
+         short ganz_kleine_zahl = 120;
+         System.out.println(Integer.MIN_VALUE);
+         System.out.println(Integer.MAX_VALUE);
+         
+         // int - 4 byte
+         int kleine_zahl = 123456;
+         System.out.println(kleine_zahl);
+         System.out.println(Integer.MIN_VALUE);
+         System.out.println(Integer.MAX_VALUE);
+         
+         // long - 8 bytes
+         long grosse_zahl = 123456789;
+         System.out.println(grosse_zahl);
+         System.out.println(Long.MIN_VALUE);
+         System.out.println(Long.MAX_VALUE);
+         
+         // float
+         float kleine_komma_zahl=123.123F; //ACHTUNG - OHNE F IST ES DOUBLE!!
+         System.out.println(kleine_komma_zahl);
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         // char 
+         char ein_zeichen = 'a';
+         System.out.println(ein_zeichen);
+         // char zwei_zeichen = 'ab' ; geht nicht.
+         
+         //string 
+         String text = "abcdefg";
+         System.out.println(text);
+         
+        		 
         
+         
+         
+        
+         
 	}
 
 }
