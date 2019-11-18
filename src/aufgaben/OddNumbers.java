905d75bac86e4446e368d00be94028f49b638791
fedorkondratev <56838391+fedorkondratev@users.noreply.github.com>
2019-11-18 18:15:32 +0100

    Update OddNumbers.java

--- src/aufgaben/OddNumbers.java
+++ src/aufgaben/OddNumbers.java
@@ -5,7 +5,7 @@ public class OddNumbers {
 	public static void main(String[] args) {
 		for (int i = 0; i <= 100; i = i + 1) {
 
-			if (i % 2 == 1) {
+			if (i % 2 == 0) {
 				System.out.println(i);
 
 			}
